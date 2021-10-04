8c6fa54ce0c2d05d6284261f6275c0ae389e376e
Khai Tran <kqtran917@yahoo.com>
2021-10-04 18:33:20 -0500

    Rendering Properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 // TODO: Render the `App` component to the DOM
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
+
+// var TestClass = (props) => (
+
+//   App
+
+// )
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
