22160e6a7f029ec64402d965aef57fa0902b53ec
Khai Tran <kqtran917@yahoo.com>
2021-10-04 22:53:39 -0500

    Implemented ES6 Class in VideoListEntry

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,11 @@
 // import exampleVideoData from './data/exampleVideoData.js';
 import VideoListEntry from './VideoListEntry.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
-var VideoList = () => (
+
+
+
+var VideoList = (exampleVideoData) => (
   <div className="video-list">
     {/* <div><h5><em>exampleVideoData</em> view goes here</h5></div>
     <div><h5><em>exampleVideoData</em> view goes here</h5></div>
@@ -21,4 +25,6 @@ VideoList.propTypes = {
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
+console.log(exampleVideoData);
+console.log(exampleVideoData[0].snippet.description);
 export default VideoList;
