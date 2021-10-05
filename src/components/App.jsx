7a59d3ca0bca832e2a728e45fa0bf2f65bdbbde9
Khai Tran <kqtran917@yahoo.com>
2021-10-04 19:25:17 -0500

    Implemented all components to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
 
 var App = () => (
   <div>
     <nav className="navbar">
       <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+        <div><h5><em>Search</em></h5><Search /></div>
       </div>
     </nav>
     <div className="row">
       <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
+        <div><h5><em>Video Player</em></h5><VideoPlayer /></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em><VideoList /></em> view goes here</h5></div>
+        <div><h5><em>Video List</em></h5><VideoList /></div>
       </div>
     </div>
   </div>
